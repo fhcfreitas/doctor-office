ed68aa563975a1f604cf1b1e0080e909a5fd3a1b
Fabio Freitas <fhcfreitas@gmail.com>
2026-04-06 12:23:47 -0300

    fix: fix seeds in test enviroment

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,46 +8,48 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-admin = User.create!(
-  email_address: "admin@example.com",
-  password: "admin",
-  admin: true,
-  password_confirmation: "admin",
-  name: "Admin User",
-  bio: "This is the admin user.",
-  city: "New York",
-  state: "NY",
-  specialty: "General Practitioner"
-)
+exit if Rails.env.test?
 
-User.create!(
-  email_address: "nonadmin@example.com",
-  password: "1234",
-  admin: false,
-  password_confirmation: "1234",
-  name: "Non-Admin User",
-  bio: "This is a non-admin user.",
-)
+admin = User.find_or_create_by!(email_address: "admin@example.com") do |u|
+  u.password = "admin"
+  u.password_confirmation = "admin"
+  u.admin = true
+  u.name = "Admin User"
+  u.bio = "This is the admin user."
+  u.city = "New York"
+  u.state = "NY"
+  u.specialty = "General Practitioner"
+end
 
-20.times do |i|
-  Post.create!(
-    user: admin,
-    title: "Post #{i + 1}: Dicas de Saúde e Bem-estar",
-    subtitle: "Aprenda algo importante sobre sua saúde ##{i + 1}",
-    content: <<~TEXT,
-      Este é o conteúdo do post número #{i + 1}.
+User.find_or_create_by!(email_address: "nonadmin@example.com") do |u|
+  u.password = "1234"
+  u.password_confirmation = "1234"
+  u.admin = false
+  u.name = "Non-Admin User"
+  u.bio = "This is a non-admin user."
+end
 
-      Aqui você pode escrever sobre diversos temas como:
-      - Prevenção de doenças
-      - Alimentação saudável
-      - Importância de check-ups regulares
-      - Qualidade de vida
+if Post.none?
+  20.times do |i|
+    Post.create!(
+      user: admin,
+      title: "Post #{i + 1}: Dicas de Saúde e Bem-estar",
+      subtitle: "Aprenda algo importante sobre sua saúde ##{i + 1}",
+      content: <<~TEXT,
+        Este é o conteúdo do post número #{i + 1}.
 
-      Manter hábitos saudáveis é essencial para uma vida longa e equilibrada.
-    TEXT
-    draft: [ true, false ].sample,
-    newsletter_flag: false,
-    newsletter_sent: false,
-    published_at: rand(1..30).days.ago
-  )
+        Aqui você pode escrever sobre diversos temas como:
+        - Prevenção de doenças
+        - Alimentação saudável
+        - Importância de check-ups regulares
+        - Qualidade de vida
+
+        Manter hábitos saudáveis é essencial para uma vida longa e equilibrada.
+      TEXT
+      draft: [ true, false ].sample,
+      newsletter_flag: false,
+      newsletter_sent: false,
+      published_at: rand(1..30).days.ago
+    )
+  end
 end
