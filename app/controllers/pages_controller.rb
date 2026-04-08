d91a10ea88f3ab7f3079f2bcd7e12c1834de1824
Fabio Freitas <fhcfreitas@gmail.com>
2026-04-08 18:41:00 -0300

    feat(pages): create home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :require_authentication, only: [ :home ]
 
   def home
+    @posts = Post.published.order(published_at: :desc).limit(3)
   end
 end
